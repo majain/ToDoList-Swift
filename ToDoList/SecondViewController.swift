1cc5b26e3d29eba2a8e33340d2ff0608ec1fc8be
Mohit Jain <mohit.jain@move.com>
2015-07-18 16:55:28 -0700

    ToDo List Project in Swift

--- ToDoList/SecondViewController.swift
+++ ToDoList/SecondViewController.swift
@@ -8,18 +8,43 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
-
+class SecondViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var addItem: UIButton!
+    
+    @IBAction func addItemToList(sender: AnyObject) {
+        
+        toDoList.append(textField.text!)
+        NSUserDefaults .standardUserDefaults() .setObject(toDoList, forKey: "toDoList")
+        
+        textField.text = ""
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.textField.resignFirstResponder()
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
+        
+        return true
+    }
+    
 }
 
